f1eec8e68429e10baa48af0592fea6b6877e2040
Joao Pedro <802.018@alunos.unigran.br>
2020-12-09 15:41:04 -0300

    Conexão com firebase

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,21 +1,33 @@
 import { render } from '@testing-library/react';
 import React , {Component} from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
-
+import firebase from './firebase';
 
 import './global.css';
 import Home from './components/Home/index';
 import Header from './components/Header';
 
 class App extends Component{
+  
+  state ={
+    firebaseInitialized: false
+  };
+  componentDidMount(){
+    firebase.isInitialized().then(resultado => {
+      this.setState({firebaseInitialized: resultado});
+    })
+  }
+
   render(){
-    return(
+    return this.state.firebaseInitialized !== false ? (
       <BrowserRouter>
       <Header></Header>
         <Switch>
           <Route exact path="/" component={Home} />
         </Switch>
       </BrowserRouter>
+    ):(
+      <h1>Carregando...</h1>
     );
   }
 }
---
+++ blog/src/firebase.js
@@ -0,0 +1,44 @@
+import app from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
+
+let firebaseConfig = {
+    apiKey: "AIzaSyCnlm6P_rwCDl_HHEHn7Q4b1mtEQ8FHPsA",
+    authDomain: "react-a9944.firebaseapp.com",
+    projectId: "react-a9944",
+    storageBucket: "react-a9944.appspot.com",
+    messagingSenderId: "1094822745579",
+    appId: "1:1094822745579:web:c7f385c64c2896deb3b5ae",
+    measurementId: "G-JM3LGL9772"
+  };
+ 
+
+class Firebase {
+constructor(){
+    app.initializeApp(firebaseConfig);
+  //  app.analytics();
+}
+
+login(email, password){
+    return app.auth().signInWithEmailAndPassword(email, password)
+}
+
+async register(nome, email, password){
+    await app.auth().createUserWithEmailAndPassword(email, password)
+
+    const uid = app.auth().currentUser.uid;
+
+    return app.database().ref('usuarios').child(uid).set({
+        nome: nome
+    })
+}
+
+isInitialized(){
+    return new Promise(resolve =>{
+        app.auth().onAuthStateChanged(resolve);
+    })
+}
+
+}
+
+export default new Firebase();
