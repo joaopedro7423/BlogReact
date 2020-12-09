e4b1e66c8bb02020de7284575f41534016b7202a
Joao Pedro <802.018@alunos.unigran.br>
2020-12-09 14:40:23 -0300

    Primeiras modificações

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,11 +1,23 @@
+import { render } from '@testing-library/react';
+import React , {Component} from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 
-function App() {
-  return (
-    <div >
-      <h1>Teste</h1>
-    </div>
-  );
+import './global.css';
+import Home from './components/Home/index';
+import Header from './components/Header';
+
+class App extends Component{
+  render(){
+    return(
+      <BrowserRouter>
+      <Header></Header>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
