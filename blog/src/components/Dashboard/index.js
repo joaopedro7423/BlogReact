50aa5a65d245d973c0487110324c5ec10fc015e9
Joao Pedro <802.018@alunos.unigran.br>
2020-12-10 10:49:41 -0300

    Painel admin Pt1

--- blog/src/components/Dashboard/index.js
+++ blog/src/components/Dashboard/index.js
@@ -1,11 +1,48 @@
 import React, { Component } from "react";
+import { Link ,withRouter } from "react-router-dom";
 
-export default class Dashboard extends Component {
+import firebase from "../../firebase";
+
+ class Dashboard extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      nome: localStorage.nome
+    };
+
+    this.logout = this.logout.bind(this);
+  }
+
+  async componentDidMount(){
+    if(!firebase.getCurrent()){
+      this.props.history.replace('/login');
+      return null;
+    }
+
+    firebase.getUserName((info)=>{
+      localStorage.nome = info.val().nome;
+      this.setState({nome: localStorage.nome});
+    })
+  }
+
+  logout(){
+
+  }
+
   render() {
     return (
-      <div>
-        <h1>Tela Painel</h1>
+      <div id="dashboard">
+        <div className="user-info">
+        <h1>Olá {this.state.nome}</h1>
+        <Link to="/dashboard/new">Novo Post</Link>
+      </div>
+      <p>Logado com: matheuzinho_gemeplays@jooj.com</p>
+      <button onClick={()=>this.logout()}>Deslogar</button>
+
       </div>
     );
   }
 }
+
+export default withRouter(Dashboard);
