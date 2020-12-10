45f6ef2b54ba4fdb38bb98dbccf165f70a32177a
Joao Pedro <802.018@alunos.unigran.br>
2020-12-10 15:50:28 -0300

    Página novos post Pt1

--- blog/src/components/New/index.js
+++ blog/src/components/New/index.js
@@ -1,13 +1,47 @@
 import React, {Component} from 'react'
 import {Link, withRouter, } from 'react-router-dom';
 
+import './new.css'
+
 class New extends Component{
+
+    constructor(props){
+        super(props);
+        this.state ={
+            titulo:'',
+            imagem:'',
+            descricao:''
+        };
+
+        this.cadastrar = this.cadastrar.bind(this);
+
+    }
+
+    cadastrar(){
+
+    }
+
     render(){
         return(
             <div>
                 <header id="new">
                     <Link to="/dashboard">voltar</Link>
                 </header>
+                <form onSubmit={this.cadastrar} id="new-post">
+                    <label>Titulo:</label>
+                    <input type="text" placeholder="Nome do post" value={this.state.titulo} autoFocus
+                    onChange={(e)=> this.setState({titulo: e.target.value})} /> <br/>
+              
+              <label>Url da imagem:</label>
+                    <input type="text" placeholder="Url da capa" value={this.state.imagem} autoFocus
+                    onChange={(e)=> this.setState({imagem: e.target.value})} /> <br/>
+              
+              <label>Descrição:</label>
+                    <textarea type="text" placeholder="Descreva aqui." value={this.state.descricao} autoFocus
+                    onChange={(e)=> this.setState({descricao: e.target.value})} /> <br/>
+              
+              <button type="submit">Cadastrar</button>
+                </form>
             </div>
         );
     }
